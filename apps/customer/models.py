0d304691e4546864caf6c312e7629cc977ef96ac
Dmytro Kolesnyk <kolesnik.dmitry16.1@gmail.com>
2023-05-18 17:30:34 +0300

    - feat(models.py): add Profile model with user and balance fields

--- apps/customer/models.py
+++ apps/customer/models.py
@@ -1,3 +1,12 @@
+from django.contrib.auth.models import User
+from django.core.validators import MinValueValidator
 from django.db import models
 
-# Create your models here.
+
+class Profile(models.Model):
+    """
+    Model representing a user profile with associated balance.
+    """
+
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    balance = models.DecimalField(max_digits=10, decimal_places=2, validators=[MinValueValidator(0)])
