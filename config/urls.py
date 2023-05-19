08cea835e2d3f7ea8e60de8ac87dd46c7b58f234
Dmytro Kolesnyk <kolesnik.dmitry16.1@gmail.com>
2023-05-19 16:03:36 +0300

    - feat(urls.py): add api_profile namespace to customer app's urls under /api/ path

--- config/urls.py
+++ config/urls.py
@@ -3,6 +3,7 @@ from django.urls import include, path
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("api/", include("apps.customer.urls", namespace="api_profile")),
 ]
 
 urlpatterns += [path("silk/", include("silk.urls", namespace="silk"))]
