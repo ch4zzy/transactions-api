904402c0e01f8237230151e47ae30e5465eb2df4
Dmytro Kolesnyk <kolesnik.dmitry16.1@gmail.com>
2023-05-19 16:05:14 +0300

    - feat(customer): add ProfileBalanceView to retrieve the current balance of a user profile. The viewset is readonly and has a single action 'get_balance' that retrieves the balance of the user profile. If the profile does not exist, an error message is returned.

--- apps/customer/views.py
+++ apps/customer/views.py
@@ -1,1 +1,27 @@
-# Create your views here.
+from rest_framework import viewsets
+from rest_framework.decorators import action
+from rest_framework.response import Response
+
+from apps.customer.models import Profile
+from apps.customer.serializer import ProfileSerializer
+
+
+class ProfileBalanceView(viewsets.ReadOnlyModelViewSet):
+    """
+    ViewSet for retrieving the current balance of a user profile.
+    """
+
+    queryset = Profile.objects.all()
+    serializer_class = ProfileSerializer
+
+    @action(detail=True, methods=["get"], url_path="balance")
+    def get_balance(self, request, pk=None):
+        """
+        Retrieves the current balance of the user profile.
+        """
+        try:
+            profile = self.get_object()
+            balance = profile.balance
+            return Response({"balance": balance})
+        except Profile.DoesNotExist:
+            return Response({"error": "Profile does not exist"})
