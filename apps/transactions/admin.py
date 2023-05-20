bf95f356ea8dc1d191131d63d9b3aaf4b010a25c
Dmytro Kolesnyk <kolesnik.dmitry16.1@gmail.com>
2023-05-20 15:29:27 +0300

    - feat(transactions): add TransactionsAdmin model admin to display transaction details in Django admin panel

--- apps/transactions/admin.py
+++ apps/transactions/admin.py
@@ -1,1 +1,16 @@
-# Register your models here.
+from django.contrib import admin
+
+from apps.transactions.models import Transactions
+
+
+@admin.register(Transactions)
+class TransactionsAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "transaction_id",
+        "transaction_type",
+        "date",
+        "amount",
+        "balance",
+        "user",
+    )
