2e1bdd0a2af006dea03975b16028d67f7a826dc7
Dmytro Kolesnyk <kolesnik.dmitry16.1@gmail.com>
2023-05-21 14:12:01 +0300

    - feat(models.py): add update_balance method to Profile model to update balance of the profile by adding the specified amount.

--- apps/customer/models.py
+++ apps/customer/models.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.models import User
 from django.core.validators import MinValueValidator
 from django.db import models
+from django.db.models import F
 
 
 class Profile(models.Model):
@@ -10,3 +11,9 @@ class Profile(models.Model):
 
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     balance = models.DecimalField(max_digits=10, decimal_places=2, validators=[MinValueValidator(0)])
+
+    def update_balance(self, balance):
+        """
+        Update the balance of the profile by adding the specified amount.
+        """
+        Profile.objects.select_for_update().filter(pk=self.pk).update(balance=F("balance") + balance)
