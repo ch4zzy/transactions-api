8ac4b704557e8ebba0ed723c20a9f125d59c323f
Dmytro Kolesnyk <kolesnik.dmitry16.1@gmail.com>
2023-05-21 14:17:28 +0300

    - feat(utils.py): add create_operation_and_update_balance function to create financial operation and update customer current balance.

---
+++ apps/transactions/utils.py
@@ -0,0 +1,14 @@
+from typing import Dict
+
+from apps.transactions.models import Transactions
+
+
+def create_operation_and_update_balance(operation_data):
+    """
+    Method for create financial operation and update customer current balance.
+    """
+    user = operation_data["user"]
+    amount = operation_data["amount"]
+    financial_operation = Transactions.objects.create(**operation_data)
+    user.profile.update_balance(amount)
+    return financial_operation
