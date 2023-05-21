131379e94720945d0370b39284b02c145b811729
Dmytro Kolesnyk <kolesnik.dmitry16.1@gmail.com>
2023-05-21 14:10:53 +0300

    - feat(urls.py): add transactions app urls to the project urlpatterns

--- config/urls.py
+++ config/urls.py
@@ -4,6 +4,7 @@ from django.urls import include, path
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/", include("apps.customer.urls", namespace="api_profile")),
+    path("api/", include("apps.transactions.urls", namespace="api_transactions")),
 ]
 
 urlpatterns += [path("silk/", include("silk.urls", namespace="silk"))]
